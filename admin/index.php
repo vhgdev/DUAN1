981cbb4ca0655c662b0bcf8d0c7a8a9c82f1439b
minh <minhptph53592.com>
2025-06-02 18:58:59 +0700

    !

--- admin/index.php
+++ admin/index.php
@@ -12,7 +12,7 @@ require_once __DIR__ . "/../models/Product.php";
 
 
 //require controllers
-// require_once __DIR__ . "/../controllers/admin/AdminProductController.php";
+require_once __DIR__ . "/../controllers/admin/AdminProductController.php";
 // require_once __DIR__ . "/../controllers/admin/AdminCategoryController.php";
 require_once __DIR__ . "/../controllers/admin/DashboardController.php";
 require_once __DIR__ . "/../controllers/AuthController.php";
@@ -23,11 +23,11 @@ require_once __DIR__ . "/../controllers/AuthController.php";
 $ctl = $_GET['ctl'] ?? "";
 
 match ($ctl) {
-    '' => (new DashboardController)->index(),
+    '' => view('admin.dashboard'),
     'listsp' => (new AdminProductController)->index(),
     'addsp' => (new AdminProductController)->create(),
     'storesp' => (new AdminProductController)->store(),
     'editsp' => (new AdminProductController)->edit(),
-    'updatesp' => (new AdminProductController)->update(),
+    // 'updatesp' => (new AdminProductController)->update(),
     'deletesp' => (new AdminProductController)->delete(),
 };
